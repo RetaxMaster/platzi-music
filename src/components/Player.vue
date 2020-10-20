cd72cc2785edec53050667da1801a0e1c7a71db0
RetaxMaster <retaxmaster@hotmail.com>
2020-10-20 16:43:04 -0500

    Before downgrade vue 2

---
+++ src/components/Player.vue
@@ -0,0 +1,42 @@
+<template>
+    
+    <div class="content">
+        <p class="is-128x128">
+            <img src="">
+        </p>
+
+        <p>
+            <strong>{{ }}</strong>
+            <small>{{ }}</small>
+        </p>
+
+        <p>
+            <audio src="" controls></audio>
+        </p>
+    </div>
+
+</template>
+
+<script>
+
+export default {
+    
+    data(){
+        return {
+            track: {}
+        }
+    },
+
+    mounted() {
+
+        /* emitter.on("set-track", (track) => {
+            console.log("Emited! ", track);
+
+            this.track = track;
+
+        }); */
+
+    }
+
+}
+</script>
